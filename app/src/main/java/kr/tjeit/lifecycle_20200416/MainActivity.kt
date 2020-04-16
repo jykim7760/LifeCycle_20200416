1c13ff08061e844092de3c27947ef96228729726
cho881020 <kj_cho@idealidea.co.kr>
2020-04-16 21:16:55 +0900

    생명주기 기초 탐구

--- app/src/main/java/kr/tjeit/lifecycle_20200416/MainActivity.kt
+++ app/src/main/java/kr/tjeit/lifecycle_20200416/MainActivity.kt
@@ -3,6 +3,7 @@ package kr.tjeit.lifecycle_20200416
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity() {
@@ -12,6 +13,15 @@ class MainActivity : BaseActivity() {
         setContentView(R.layout.activity_main)
         setupEvents()
         setValues()
+
+        Log.d("MainActivity", "onCreate실행됨")
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        Log.d("MainActivity", "onResume실행됨")
+
     }
 
     override fun setupEvents() {
