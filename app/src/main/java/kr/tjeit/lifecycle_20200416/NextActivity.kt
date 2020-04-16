a573dcc0574238742a341339138bea55b8212868
cho881020 <kj_cho@idealidea.co.kr>
2020-04-16 21:09:37 +0900

    다음 화면 이동 기능 추가

--- app/src/main/java/kr/tjeit/lifecycle_20200416/NextActivity.kt
+++ app/src/main/java/kr/tjeit/lifecycle_20200416/NextActivity.kt
@@ -1,32 +1,23 @@
 package kr.tjeit.lifecycle_20200416
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : BaseActivity() {
+class NextActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_next)
         setupEvents()
         setValues()
     }
 
     override fun setupEvents() {
 
-        nextBtn.setOnClickListener {
-            val myIntent = Intent(mContext, NextActivity::class.java)
-            startActivity(myIntent)
-        }
-
     }
 
     override fun setValues() {
 
     }
-
-
 
 }
